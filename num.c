a3965acf9ef496ab3424e94ab9ec86485e677cbc
reshmirnairr <reshmirnair590@gmail.com>
2022-11-15 10:19:58 +0530

    my first pogram

---
+++ num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int add(int,int);
+int main()
+{
+int a,b,sum;
+printf("Enter first number");
+scanf("%d",&a);
+printf("Enter second number");
+scanf("%d",&b);
+sum=add(a,b);
+printf("The sum is %d\n",sum);
+return 0;
+}
+int add(int x,int y)
+{
+return (x+Y);
+}
---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum;
+printf("Enter  number:");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("sum=%d\n",sum);
+}
